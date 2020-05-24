b47fe2f29e24d82d3e411ccd754c2654c798c0ad
CodePhisch <codephisch@gmail.com>
2020-05-24 15:43:41 +0200

    Praktikum 5 hinzugefügt

---
+++ src/praktikum5/Medienverwaltung.java
@@ -0,0 +1,102 @@
+package praktikum5;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+public class Medienverwaltung {
+	private List<Medium> medien;
+	
+	public Medienverwaltung() {
+		medien = new ArrayList<Medium>();
+	}
+	
+	public void aufnehmen(Medium medium) {
+		medien.add(medium);
+	}
+	
+	public void zeigeMedien() {
+		Collections.sort(medien);
+		OutputStream os = new PrintStream(System.out);
+		System.out.println("--- Alle Medien ---");
+		for(Medium m : medien) {
+			m.druckeDaten(os);
+		}
+		System.out.println("--- ----------- ---");
+	}
+	
+	public void sucheNeuesMedium() {
+		Collections.sort(medien);
+		OutputStream os = new PrintStream(System.out);
+		System.out.println("--- Neuestes Medium ---");
+		medien.get(medien.size() - 1).druckeDaten(os);
+		System.out.println("--- --------------- ---");
+	}
+	
+	public double berechneErscheinungsjahr() {
+		int summe = 0;
+		
+		for(Medium m : medien) {
+			summe += m.getJahr();
+		}
+		
+		if(medien.size() > 0) {
+			return (double) summe / medien.size();
+		} else {
+			return 0.0;
+		}
+	}
+	
+	public void writeListToFile() {
+		Collections.sort(medien);
+		File file;
+		boolean validFile = false;
+		
+		do {
+			try {
+				file = readFile();
+				if(file != null) {
+					try {
+						OutputStream os = new FileOutputStream(file);
+						for(Medium m : medien) {
+							m.druckeDaten(os);
+						}
+						return;
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				} else {
+					System.out.println("Fehler beim Dateizugriff!");
+				}
+			} catch (EmptyFilenameException e) {
+				int auswahl = JOptionPane.showConfirmDialog(null, "Der Dateiname darf nicht leer sein! Neuen eingeben?", "Fehlerhafte Eingabe", JOptionPane.YES_NO_OPTION);
+				if(auswahl == 1) {
+					System.out.println("Eingabe abgebrochen wegen leerem Dateipfad.");
+					return;
+				}
+			} catch (AbbruchException e) {
+				System.out.println("Eingabe des Dateinamens abgebrochen.");
+				return;
+			} 
+		} while(!validFile);
+		
+	}
+	
+	public File readFile() throws EmptyFilenameException, AbbruchException {
+		String path = JOptionPane.showInputDialog("Bitte geben Sie den Dateipfad ein");
+		
+		if(path == null) throw new AbbruchException("Eingabe des Dateinamens abgebrochen.");
+		if(path.isEmpty()) throw new EmptyFilenameException("Leerer Dateiname");
+		
+		File file = new File(path);
+		if(file.isFile()) return file;
+		return null;
+	}
+}
---
+++ src/praktikum5/Medium.java
@@ -0,0 +1,69 @@
+package praktikum5;
+
+import java.io.OutputStream;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public abstract class Medium implements Comparable<Medium> {
+	public static int anzahlMedien = 0;
+	
+	private int id;
+	private String titel;
+	private int jahr;
+	
+	public Medium(String titel, int jahr) {
+		this.titel = titel;
+		this.jahr = jahr;
+		
+		id = anzahlMedien++;
+	}
+	
+	public int alter() {
+		int aktuellesJahr = LocalDate.now().getYear();
+		return aktuellesJahr - jahr;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public String getTitel() {
+		return titel;
+	}
+
+	public int getJahr() {
+		return jahr;
+	}
+
+	public abstract void druckeDaten(OutputStream os);
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj)
+			return true;
+		
+		if(obj == null)
+			return false;
+		
+		if(this.getClass() != obj.getClass())
+			return false;
+		
+		Medium medium = (Medium) obj;
+		
+		if(!(Objects.equals(this.getTitel(), medium.getTitel())
+				&& Objects.equals(this.getJahr(), medium.getJahr())))
+			return false;
+		
+		return true;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(titel, jahr);
+	}
+	
+	@Override
+	public int compareTo(Medium m) {
+		return Integer.compare(this.jahr, m.getJahr());
+	}
+}
