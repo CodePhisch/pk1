b2b786118988eb9828099d6028c914b1b596c929
CodePhisch <codephisch@gmail.com>
2020-06-03 10:50:14 +0200

    Praktikum 6 hinzugefügt

---
+++ src/praktikum6/Audio.java
@@ -0,0 +1,48 @@
+package praktikum6;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Objects;
+
+public class Audio extends Medium {
+
+	private String interpret;
+	private int dauer;
+	
+	public Audio(String titel, int jahr, String interpret, int dauer) {
+		super(titel, jahr);
+		
+		this.interpret = interpret;
+		this.dauer = dauer;
+	}
+
+	public String getInterpret() {
+		return interpret;
+	}
+
+	public int getDauer() {
+		return dauer;
+	}
+
+	@Override
+	public void druckeDaten(OutputStream os) {
+		PrintStream ps = new PrintStream(os);
+		ps.printf("ID = %d \"%s \" von %s aus %d Spieldauer: %d sek.\n", this.getId(), this.getTitel(), this.getInterpret(), this.getJahr(), this.getDauer());
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(super.equals(obj)) {
+			Audio audio = (Audio) obj;
+			return Objects.equals(interpret, audio.getInterpret())
+					&& Objects.equals(dauer, audio.getDauer());
+		} 
+		
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return super.hashCode() + Objects.hash(interpret, dauer);
+	}
+}
---
+++ src/praktikum6/Bild.java
@@ -0,0 +1,43 @@
+package praktikum6;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.Objects;
+
+public class Bild extends Medium {
+
+	private String ort;
+	
+	public Bild(String titel, int jahr, String ort) {
+		super(titel, jahr);
+		
+		this.ort = ort;
+	}
+
+	public String getOrt() {
+		return ort;
+	}
+	
+	@Override
+	public void druckeDaten(OutputStream os) {
+		PrintStream ps = new PrintStream(os);
+		ps.printf("ID = %d \"%s \" aufgenommen im Jahr %d in %s\n", this.getId(), this.getTitel(), this.getJahr(), this.getOrt());
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(super.equals(obj)) {
+			Bild bild = (Bild) obj;
+			return Objects.equals(ort, bild.getOrt());
+		} 
+		
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return super.hashCode() + Objects.hash(ort);
+	}
+}
+
